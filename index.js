027cb515c653bff9784f1cb43cf87fc24d7ad200
MohammadFaysal731 <www.faysal731@gmail.com>
2022-05-21 00:07:11 +0600

    start 77 Module

--- index.js
+++ index.js
@@ -32,6 +32,20 @@ function verifyJWT(req, res, next) {
 }
 
 
+// const mailchimp = require("@mailchimp/mailchimp_marketing");
+
+// mailchimp.setConfig({
+//     apiKey: "YOUR_API_KEY",
+//     server: "YOUR_SERVER_PREFIX",
+// });
+
+// async function run() {
+//     const response = await mailchimp.ping.get();
+//     console.log(response);
+// }
+
+// run();
+
 
 async function run() {
 
