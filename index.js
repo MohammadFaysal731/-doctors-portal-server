88fa120aba73311a1a67c9ce4102b2385bd7b504
MohammadFaysal731 <www.faysal731@gmail.com>
2022-05-21 13:47:49 +0600

    finesed 77_5

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -158,9 +158,16 @@ async function run() {
                 return res.status(403).send({ message: 'Forbidden Access' })
             }
 
+        });
+
+        app.get('/booking/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const booking = await bookingCollection.findOne(query);
+            res.send(booking);
         })
 
-        app.post('/booking', async (req, res) => {
+        app.post('/booking', verifyJWT, async (req, res) => {
             const booking = req.body;
             const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient }
             const exists = await bookingCollection.findOne(query);
