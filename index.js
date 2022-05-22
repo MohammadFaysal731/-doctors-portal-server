5f58952efda4f4f31b561316ba3aa4d7e42c9ece
MohammadFaysal731 <www.faysal731@gmail.com>
2022-05-22 18:46:37 +0600

    create faysal api

--- index.js
+++ index.js
@@ -194,6 +194,10 @@ async function run() {
             const filter = { email: email }
             const result = await doctorCollection.deleteOne(filter);
             res.send(result);
+        });
+
+        app.get('/faysal', async (req, res) => {
+            res.send('hello Faysla')
         })
 
     }
